774e853137ea094aed4434ffbddbc181f53e50ff
Michele Castrovilli <michele.castrovilli@gmail.com>
2025-12-18 15:42:41 +0100

    slackcheck: Fix for copyright, forgot to change it

--- eBPF/src/slackcheck.bpf.c
+++ eBPF/src/slackcheck.bpf.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
-/* Copyright (c) 2020 Facebook */
+/* Copyright (c) 2025 Michele Castrovilli <michele.castrovilli@unito.it> */
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
